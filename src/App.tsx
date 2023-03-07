c0d1a4f8351c8266646520412c0a6fc0f9dddcb7
Sudan Shakya <sudanshakya08@gmail.com>
2023-03-07 18:46:54 +0545

    initialized project

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'; 
+import Login from './pages/Login'
+import { ChakraProvider } from '@chakra-ui/react';
+
+const App = () => {
+    return(
+        <>
+        <ChakraProvider>
+        <BrowserRouter>
+        <Routes>
+        <Route path={"/"} element={<Login />}></Route>
+        </Routes>
+        </BrowserRouter>
+        </ChakraProvider>
+        </>
+    
+    )
+}
+
+export default App
